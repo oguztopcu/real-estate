004f1956da138a5db9f27edaba03e0d8da98aed6
Oguz Topcu <oguz@mynextmatch.com>
2021-09-08 00:45:58 +0300

    user model was added on dependency and also create method was added

--- app/Repositories/Users/UserRepository.php
+++ app/Repositories/Users/UserRepository.php
@@ -2,7 +2,34 @@
 
 namespace App\Repositories\Users;
 
+use App\Models\User;
+
 class UserRepository
 {
+    /**
+     * @var User $model
+     */
+    private $model;
 
+    /**
+     * @param User $user
+     */
+    public function __construct(User $user)
+    {
+        $this->model = $user;
+    }
+
+    /**
+     * @param array $request
+     * @return mixed
+     */
+    public function create(array $request): User
+    {
+        return $this->model->query()->create([
+            'name' => $request['name'],
+            'surname' => $request['surname'],
+            'email' => $request['email'],
+            'password' => bcrypt($request['password'])
+        ]);
+    }
 }
