783033d831ed48623d85a50c020cb8f31598a3fc
Oguz Topcu <oguz@mynextmatch.com>
2021-09-08 00:45:47 +0300

    user repository was added on dependency and also create method was added

--- app/Managers/UserManager.php
+++ app/Managers/UserManager.php
@@ -2,7 +2,19 @@
 
 namespace App\Managers;
 
+use App\Repositories\Users\UserRepository;
+
 class UserManager
 {
+    private $userRepository;
 
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function create(array $request)
+    {
+        return $this->userRepository->create($request);
+    }
 }
