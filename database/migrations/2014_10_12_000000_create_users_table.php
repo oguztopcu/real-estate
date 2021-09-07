dc0664b86b7e91a6ca42542e3438c01c32f2b15c
Oguz Topcu <oguz@mynextmatch.com>
2021-09-08 00:43:55 +0300

    name, surname, email and password fields has been added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,11 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('surname');
+            $table->string('email')->unique()->index();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->timestampsTz();
+            $table->softDeletesTz();
         });
     }
 
