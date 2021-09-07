08fef99fad321277628ceae17a6d3ed26be79e57
Oguz Topcu <oguz@mynextmatch.com>
2021-09-08 02:05:31 +0300

    Soft delete was added also guarded and dates property was added

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Contact extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = ['id'];
+
+    protected $dates = ['deleted_at'];
 }
