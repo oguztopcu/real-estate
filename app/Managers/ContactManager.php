fd181f11bf5e53cba934ccd3501078648601da7f
Oguz Topcu <oguz@mynextmatch.com>
2021-09-08 02:05:52 +0300

    CRUD method has been added

--- app/Managers/ContactManager.php
+++ app/Managers/ContactManager.php
@@ -2,7 +2,58 @@
 
 namespace App\Managers;
 
+use App\Models\Contact;
+use App\Repositories\Contacts\ContactRepository;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
 class ContactManager
 {
+    /**
+     * @var ContactRepository $contactRepository
+     */
+    private $contactRepository;
 
+    /**
+     * @param ContactRepository $contactRepository
+     */
+    public function __construct(ContactRepository $contactRepository)
+    {
+        $this->contactRepository = $contactRepository;
+    }
+
+    /**
+     * @return Collection
+     */
+    public function all(): Collection
+    {
+        return $this->contactRepository->all();
+    }
+
+    /**
+     * @param array $request
+     * @return Model
+     */
+    public function create(array $request)
+    {
+        return $this->contactRepository->create($request);
+    }
+
+    /**
+     * @param Contact $contact
+     * @param array $request
+     * @return Contact
+     */
+    public function update(Contact $contact, array $request = []): Contact
+    {
+        return $this->contactRepository->update($contact, $request);
+    }
+
+    /**
+     * @param Contact $contact
+     */
+    public function delete(Contact $contact)
+    {
+        $this->contactRepository->delete($contact);
+    }
 }
--- app/Repositories/Contacts/ContactRepository.php
+++ app/Repositories/Contacts/ContactRepository.php
@@ -2,7 +2,64 @@
 
 namespace App\Repositories\Contacts;
 
+use App\Models\Contact;
+use Illuminate\Database\Eloquent\Collection;
+
 class ContactRepository
 {
+    /**
+     * @var Contact $model
+     */
+    private $model;
 
+    /**
+     * @param Contact $contact
+     */
+    public function __construct(Contact $contact)
+    {
+        $this->model = $contact;
+    }
+
+    /**
+     * @return Collection
+     */
+    public function all(): Collection
+    {
+        return $this->model->query()->get();
+    }
+
+    public function create(array $request)
+    {
+        return $this->model->query()->create([
+            'name' => $request['name'],
+            'surname' => $request['surname'],
+            'email' => $request['email'],
+            'phone' => $request['phone']
+        ]);
+    }
+
+    /**
+     * @param Contact $contact
+     * @param array $request
+     * @return Contact
+     */
+    public function update(Contact $contact, array $request): Contact
+    {
+        $contact->update([
+            'name' => $request['name'],
+            'surname' => $request['surname'],
+            'email' => $request['email'],
+            'phone' => $request['phone']
+        ]);
+
+        return $contact;
+    }
+
+    /**
+     * @param Contact $contact
+     */
+    public function delete(Contact $contact): void
+    {
+        $contact->delete();
+    }
 }
