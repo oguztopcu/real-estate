39f4fad88154a73e97dd7331d49760cd9a17db74
Oguz Topcu <oguz@mynextmatch.com>
2021-09-08 01:16:52 +0300

    login request validation class

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
     Route::post('/register', [RegisterController::class, 'store']);
+
+    Route::post('/login', [LoginController::class, 'login']);
+
+    Route::post('/logout', [LoginController::class, 'logout']);
+
+    Route::post('/refresh-token', [LoginController::class, 'refreshToken']);
 });
