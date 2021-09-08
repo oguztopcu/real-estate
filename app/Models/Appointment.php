c70b2c6ef67a2df154880a3a16a25106c373a460
Oguz Topcu <oguz@mynextmatch.com>
2021-09-08 13:33:00 +0300

    Soft delete trait was added and dates property was added also contact relation has been added

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Appointment extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = ['id'];
+
+    protected $dates = [
+        'est_departure_date', 'est_arrival_date', 'deleted_at'
+    ];
+
+    public function contact(): BelongsTo
+    {
+        return $this->belongsTo(Contact::class);
+    }
 }
